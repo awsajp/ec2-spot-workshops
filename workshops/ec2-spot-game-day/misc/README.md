85a068f652f099af3427045557c2920d02a5b1ec
Alawala <jalawala@amazon.com>
2020-06-25 07:14:08 +0530

    Update

---
+++ workshops/ec2-spot-game-day/misc/README.md
@@ -0,0 +1,162 @@
+# aws_ec2_spot
+This repository contains all the code related to EC2 Spot
+
+https://aws.amazon.com/blogs/machine-learning/train-deep-learning-models-on-gpus-using-amazon-ec2-spot-instances/
+
+Shell commands
+
+aws ec2 run-instances \
+    --image-id ami-0027dfad6168539c7 \
+    --security-group-ids <SECURITY_GROUP_ID> \
+    --count 1 \
+    --instance-type m4.xlarge \
+    --key-name <KEYPAIR_NAME> \
+    --subnet-id <SUBNET_ID> \
+    --query "Instances[0].InstanceId"
+    
+
+aws ec2 create-volume \
+    --size 100 \
+    --region <AWS_REGION> \
+    --availability-zone <INSTANCE_AZ> \
+    --volume-type gp2 \
+    --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=DL-datasets-checkpoints}]' 
+
+aws ec2 attach-volume \
+    --volume-id vol-<your_volume_id> \
+    --instance-id i-<your_instance_id> \
+    --device /dev/sdf
+        
+        
+sudo mkdir /dltraining
+sudo mkfs -t xfs /dev/xvdf
+sudo mount /dev/xvdf /dltraining
+sudo chown -R ubuntu: /dltraining/
+cd /dltraining
+mkdir datasets
+mkdir checkpoints        
+
+
+aws ec2 terminate-instances \
+    --instance-ids i-<your_instance_id> \
+    --output text
+    
+
+sudo mkdir -p /rootvolume/test1
+sudo su
+echo  "this is a test for root volume " > /rootvolume/test1/file1.txt
+
+
+sudo mkdir /volume1
+sudo mkfs -t xfs /dev/xvdb
+sudo mount /dev/xvdb /volume1
+sudo chown -R ec2-user: /volume1/
+mkdir -p /volume1/test1
+echo  "this is a test for volume1" > /volume1/test1/file1.txt
+
+
+sudo mkdir /volume2
+sudo mkfs -t xfs /dev/xvdc
+sudo mount /dev/xvdc /volume2
+sudo chown -R ec2-user: /volume2/
+mkdir -p /volume2/test2
+echo  "this is a test for volume2" > /volume2/test2/file2.txt
+
+
+i-026bb75f4bdd1e53a
+
+vol-07770c367e2baa282  root
+ami-02dd77165c07f3843
+vol-07a00fdd16a96dc3f vol1  snap-0fc8c2a86174ad86a
+
+vol-06aa4fed8807d0fcb  vol2 snap-06c7304a47f9953f1
+
+
+
+m = open("o", "r").read()
+
+
+
+with open('o') as f:
+  data = json.load(f)
+print(data)
+  
+print(json.dumps(data, indent=2))
+
+print(json.dumps(data['Instances'][0], indent=2))  
+
+
+
+if new instance launched
+  create 2 and 3 volumes
+  
+if spot interruption
+
+   create snapshot from volume
+   create an AMI for root from snapshot
+   if same AZ
+	   attach additiional volumes
+   else
+       create snapshots
+	   create volume
+	   attach
+	   delete volume
+	   
+       
+   create AMI takes time
+   delete the volumes
+   launch new AMI from this instance
+   
+import boto3
+ec2client = boto3.client('ec2', region_name='us-east-1')
+instanceId = 'i-02994c99e6a6c052c'
+describeInstance = ec2client.describe_instances(InstanceIds=[instanceId])
+print(json.dumps(describeInstance, indent=2, default=json_util.default))
+
+       
+periodic   
+s1 s2 s3
+
+create an AMI
+
+
+
+
+
+print(json.dumps(m))
+  '"Instances": [
+    {
+      "LaunchTemplateAndOverrides": {
+        "LaunchTemplateSpecification": {
+          "LaunchTemplateId": "lt-0e10e9c8156945055",
+          "Version": "1"
+        },
+        "Overrides": {
+          "InstanceType": "m4.large",
+          "SubnetId": "subnet-01e89d5cc1b12f515",
+          "WeightedCapacity": 1.0
+        }
+      },
+      "Lifecycle": "spot",
+      "InstanceIds": [
+        "i-03af99d353d07c0a3"
+      ],
+      "InstanceType": "m4.large"
+    }
+  ]'
+  
+  
+10.0.0.187
+
+import base64
+
+10.0.0.146
+
+data = open("sample.txt", "r").read()
+encoded = base64.b64encode(data)
+message = "Python is fun"
+message_bytes = message.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
+
+print(base64_message)
--- workshops/ec2-spot-windows-integration/lambda_function.py
+++ workshops/ec2-spot-windows-integration/lambda_function.py
@@ -1,34 +1,328 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 import os
+import json
+import decimal
+import time
+from random import seed
+from random import random
+import threading
 
 
-def handler(event, context):
-  print("This is test code1")
+HostsTableName=os.getenv('DYNAMODB_HOST_TABLE_NAME')
+InstancesTableName=os.getenv('DYNAMODB_INSTANCEID_TABLE_NAME')
+StatusIndexName=os.getenv('DYNAMODB_GSI_ST')
+InstanceIndexName=os.getenv('DYNAMODB_GSI_InstanceId')
+DC1InstanceId=os.getenv('DC1_INSTANCE_ID')
 
-  print("event={} context={}".format(str(event), str(context)))
+dynamodb = boto3.resource('dynamodb')
+HostsTable = dynamodb.Table(HostsTableName)
+InstanceTable = dynamodb.Table(InstancesTableName)
+client = boto3.client('ssm')
 
-  client = boto3.client('autoscaling')
-  asg_name=os.getenv('ASG_NAME')
-  print("asg_name={}".format(asg_name))
-  response = client.describe_auto_scaling_groups(
-      AutoScalingGroupNames=[
-        asg_name,
-      ]
-    )
-  DesiredCapacity=response['AutoScalingGroups'][0]['DesiredCapacity']
-  
+def lambda_handler(event, context):
+
+  instanceId = event['detail']['instance-id']
+
+  print("instanceId={} HostsTableName={}, InstancesTableName={}, StatusIndexName={}, InstanceIndexName={} DC1InstanceId={}".format(instanceId, HostsTableName,InstancesTableName, StatusIndexName, InstanceIndexName, DC1InstanceId))
+
+
   if event["detail-type"] == "EC2 Spot Instance Interruption Warning":
-    DesiredCapacity += 1
-    print("Received EC2 Spot Instance Interruption Warning. Increasing DesiredCapacity from {} to {}".format(DesiredCapacity-1, DesiredCapacity))
+    print("Received EC2 Spot Instance Interruption Warning for instanceId={}...".format(instanceId))
+
+    # Get the host name for this interrupted spot instance.
+    resp = HostsTable.query(
+      IndexName=InstanceIndexName,
+      KeyConditionExpression=Key('InstanceId').eq(instanceId),
+    )
+
+    if resp['Count'] >=1 :
+
+      interruptedHostId = resp['Items'][0]["Host"]
+      interruptedHostName = resp['Items'][0]["HostName"]
+      L=interruptedHostName.split('-')
+      interruptedHostNewName=L[0]+"-"+str(int(L[1])+1)
+
+      HostsTable.update_item(
+        Key={
+          'Host': interruptedHostId
+        },
+        UpdateExpression='SET ST = :val1, InstanceId = :val2, HostName = :val3',
+        ExpressionAttributeValues={
+          ':val1': 'FREE',
+          ':val2': "Empty",
+          ':val3': interruptedHostNewName
+        }
+      )
+
+
+      print("interruptedHostId={} interruptedHostName={} interruptedHostNewName={}".format(interruptedHostId, interruptedHostName, interruptedHostNewName))
+
+      print("Sending an interruption message to all the users for the hostname={} with nstanceId={}".format(interruptedHostName, instanceId))
+      document = 'AWS-RunPowerShellScript'
+      c="C:\\windows\\system32\\msg.exe * Hello,  This desktop will be shutdown in 120 seconds. Please save your work immediately. You will lose unsaved work, when this desktop is shutdown.  A new desktop will be made available to you to login into shortly. Your new Desktop host name is " + str(interruptedHostNewName)
+      params= { "commands": [c] }
+      send_command_to_the_instance(instanceId, document, params )
+
+
+      #   detach EBS volume
+
+      #(available, nextFreeInstance) = get_next_available_resource("instance", instanceId)
+
+      available=True
+      nextFreeInstance = "i-0cc24c8b57c3ab098"
+
+      # check if there is at least one instance available in the buffer pool
+      if available :
+
+        print("Assigning  nextFreeInstance={} to interruptedHostNewName ID={} in HostsTableName={} for interruptedHostId={} ".format(nextFreeInstance, interruptedHostNewName, HostsTableName, interruptedHostId))
+        wait_ec2_complate(nextFreeInstance)
+        print("calling add_instance_to_domain with nextFreeInstance = {} to interruptedHostNewName ID = {}".format(nextFreeInstance, interruptedHostNewName))
+        add_instance_to_domain(nextFreeInstance, interruptedHostNewName)
+
+
+        print("Assigning  nextFreeInstance={} to interruptedHostNewName ID={} in HostsTableName={} for interruptedHostId={} ".format(nextFreeInstance, interruptedHostNewName, HostsTableName, interruptedHostId))
+        HostsTable.update_item(
+          Key={
+            'Host': interruptedHostId
+          },
+          UpdateExpression='SET ST = :val1, InstanceId = :val2, HostName = :val3',
+          ExpressionAttributeValues={
+            ':val1': 'ASSIGNED',
+            ':val2': nextFreeInstance,
+            ':val3': interruptedHostNewName
+          }
+        )
+
+      else:
+        # There is no instance available in the pool. So change the status of the instance from ASSIGNED to FREE
+
+        print("Assigning FREE status to  interruptedHostName={} HostsTableName={} since there are no free instances in the buffer pool".format(interruptedHostName, HostsTableName))
+        HostsTable.update_item(
+          Key={
+            'Host': interruptedHostId
+          },
+          UpdateExpression='SET ST = :val1, HostName = :val2',
+          ExpressionAttributeValues={
+            ':val1': 'FREE',
+            ':val2': interruptedHostNewName
+          }
+        )
+
+      remove_instance_to_domain(instanceId, interruptedHostName)
+
+    else:
+      print("Removing instanceId={} to buffer pool in InstancesTableName={} since it is interrupted and NOT assigned to any Host".format(instanceId, InstancesTableName))
+
+      response = InstanceTable.delete_item(
+        Key={
+          'InstanceId': instanceId
+        }
+      )
+
+
   elif event["detail-type"] == "EC2 Spot Instance Request Fulfillment":
-    DesiredCapacity -= 1
-    print("Received EC2 Spot Instance Request Fulfillment. Decreasing DesiredCapacity  from {} to {}".format(DesiredCapacity+1, DesiredCapacity))
+    print("Received EC2 Spot Instance Request Fulfillment... for instanceId={}".format(instanceId))
 
-  response = client.set_desired_capacity(
-    AutoScalingGroupName=asg_name,
-    DesiredCapacity=DesiredCapacity,
-    HonorCooldown=True
+    # Check if any Host is FREE and need to be assignedE
+    (available, freeHostName) = get_next_available_resource("host", instanceId)
+
+    # check if there is at least one Host which is FREE
+
+    if available :
+
+      wait_ec2_complate(instanceId)
+      print("Calling add_instance_to_domain with freeHostName = {} to Instance ID = {}".format(freeHostName, instanceId))
+      add_instance_to_domain(instanceId, freeHostName)
+
+    else:
+
+      #Add the instance to the buffer pool
+      print("Adding instanceId={} to buffer pool in InstancesTableName={}".format(instanceId, InstancesTableName))
+      response = InstanceTable.put_item(
+        Item={
+          'InstanceId': instanceId,
+          'ST': 'FREE',
+          'IP': '5.5.5.5'
+        }
+      )
+
+
+def get_next_available_resource(name, instanceId):
+
+  retValue = "dummy"
+  index = "dummy"
+  value1="dumm"
+  value2="dummy"
+  seed(1)
+  if name == "host":
+    min=0
+    max=60
+    table=HostsTable
+  else:
+    min=0
+    max=10
+    table=InstanceTable
+
+  value1=min + (random() * (max - min))
+  time.sleep(value1)
+
+  resp = table.query(
+    IndexName=StatusIndexName,
+    KeyConditionExpression=Key('ST').eq('FREE'),
   )
-  
 
-  return
+  if resp['Count'] >=1 :
+    available = True
+    print("The following Hosts are FREE and needs to be assigned:")
+    for item in resp['Items']:
+      print(item)
+
+    min=0
+    max=resp['Count']
+    value2=min + (random() * (max - min))
+    index=int(value2)
+
+
+
+    if name == "host":
+      HostId = resp['Items'][index]["Host"]
+      freeHostName = resp['Items'][index]["HostName"]
+
+      print("Assigning  instanceId={} to freeHostName ID={} in HostsTableName={} for HostId={} ".format(instanceId, freeHostName, HostsTableName, HostId))
+      HostsTable.update_item(
+        Key={
+          'Host': HostId
+        },
+        UpdateExpression='SET ST = :val1, InstanceId = :val2',
+        ExpressionAttributeValues={
+          ':val1': 'ASSIGNED',
+          ':val2': instanceId
+        }
+      )
+      retValue = freeHostName
+    else:
+      nextFreeInstance = resp['Items'][index]["InstanceId"]
+      print("Removing nextFreeInstance={} from the buffer pool in InstancesTableName={}".format(nextFreeInstance, InstancesTableName))
+      response = InstanceTable.delete_item(
+        Key={
+          'InstanceId': nextFreeInstance
+        }
+      )
+      retValue = nextFreeInstance
+
+  else:
+    available = False
+
+  print("name={} value1={} available={} count={} value2={} index={} retValue={}".format(name, value1,  str(available), resp['Count'],value2, index, retValue))
+  return (available, retValue)
+
+def remove_instance_to_domain(instanceId, interruptedHostName):
+  #L=args.split(':')
+  #instanceId=L[0]
+  #interruptedHostName=L[1]
+
+  print("Remove-Computer() called for instanceId={}".format(instanceId))
+  document = 'AWS-RunPowerShellScript'
+  c="Remove-Computer -Force  -Restart "
+  #c="Remove-Computer -Force "
+  params= { "commands": [c] }
+  send_command_to_the_instance(instanceId, document, params )
+  time.sleep (45)
+
+  #remove the computer from the AD
+  print("Remove-ADComputer() called for interruptedHostName={} from DC1InstanceId={}".format(interruptedHostName, DC1InstanceId))
+  document = 'AWS-RunPowerShellScript'
+  c1="Remove-ADComputer -Identity   "+str(interruptedHostName) + "  -confirm:$false "
+  c2="Remove-DnsServerResourceRecord -Force -ZoneName example.com -RRType A -Name "  + str(interruptedHostName)
+  params= { "commands": [c1, c2] }
+  send_command_to_the_instance(DC1InstanceId, document, params )
+  #time.sleep (20)
+
+def add_instance_to_domain(instanceId, hostname):
+
+  print("add_instance_to_domain:rename instanceId={} hostname={}".format(instanceId, hostname))
+
+  #assign_hostname_to_instance(instanceId, freeHostName)
+  document = 'AWS-RunPowerShellScript'
+  #part1= "{ \"commands\": [" + " \"Rename-Computer -NewName   "
+  #part2=str(freeHostName) + "  \" ], }"
+  #params =  part1 + part2
+  c="Rename-Computer -NewName     "+str(hostname)
+  params= { "commands": [c] }
+  send_command_to_the_instance(instanceId, document, params)
+
+  time.sleep (20)
+
+  # Join the Host to the domain
+
+  #add_instance_to_domain(instanceId)
+  print("add_instance_to_domain:domain instanceId={} hostname={}".format(instanceId, hostname))
+  document = 'awsconfig_Domain_d-9367157267_example.com'
+  params={}
+  send_command_to_the_instance(instanceId, document, params )
+
+  time.sleep (20)
+  #wait for few seconds to ensure domain join is successful
+
+  # Restart the Host to make the changes effect
+  #restart_instance(instanceId)
+  print("add_instance_to_domain:restart instanceId={} hostname={}".format(instanceId, hostname))
+  document = 'AWS-RunPowerShellScript'
+  #params="{ \"commands\": [" + " \"Restart-Computer -Force  \"  " + "   ], }"
+  c="Restart-Computer -Force"
+  params= { "commands": [c] }
+  send_command_to_the_instance(instanceId, document, params )
+  time.sleep (20)
+
+
+
+def send_command_to_the_instance(instanceId, document, params):
+
+  #print("instanceId={}".format(instanceId))
+
+
+  client = boto3.client('ssm')
+  response = client.send_command(
+    Targets=[
+      {
+        'Key': 'InstanceIds',
+        'Values': [
+          str(instanceId),
+        ]
+      },
+    ],
+    DocumentName=document,
+    DocumentVersion='1',
+    TimeoutSeconds=300,
+    Comment="sending a command command to the PC",
+    Parameters=params,
+    MaxConcurrency='50',
+    MaxErrors='0'
+  )
+  print("response={}".format(response))
+
+def wait_ec2_complate(instance_id):
+
+  #print("instance_id={}".format(instance_id))
+  client = boto3.client('ec2')
+  iter = 1
+  status = False
+  while True:
+    iter += 1
+    time.sleep(15)
+    rsp = client.describe_instance_status(
+      InstanceIds=[str(instance_id)],
+      IncludeAllInstances=True
+    )
+    # double check 2/2 status
+    instance_status = rsp['InstanceStatuses'][0]['InstanceStatus']['Status']
+    system_status = rsp['InstanceStatuses'][0]['SystemStatus']['Status']
+    print("iter={} Instance status is {} System status is {}".format(iter, str(instance_status), str(system_status)))
+    if str(instance_status) == 'ok' and str(system_status) == 'ok':
+      status = True
+      break
+
+    if iter >= 30:
+      break
+  return status
